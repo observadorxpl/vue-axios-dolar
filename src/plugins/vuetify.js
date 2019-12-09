71471d21ab3a25f7b2024d9483d21da3a6457b80
Jose Luis Cayo <observadorxpl@gmail.com>
2019-12-09 01:38:06 -0500

    Tema de vuetify cambiado

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,13 +11,13 @@ export default new Vuetify({
       },
     themes: {
       light: {
-        primary: '#ee44aa',
+        primary: '#1976D2',
         secondary: '#424242',
         accent: '#82B1FF',
         error: '#FF5252',
         info: '#2196F3',
         success: '#4CAF50',
-        warning: '#FFC107'
+        warning: '#FFC107',
       },
     },
   },
